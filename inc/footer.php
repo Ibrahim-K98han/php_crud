e3edc331d7afe3dbf9b1576953dd6c9c0f3ae028
Ibrahim Khan <aitvetibrahim@gmail.com>
2023-11-02 20:47:30 +0600

    first commit

---
+++ inc/footer.php
@@ -0,0 +1,16 @@
+</div>
+    <footer class="footeroption">
+        <section class="footerone">
+            <p>Ibrahim Khan</p>
+            <p>CSE Student</p>
+            <p>BUBT</p>
+        </section>
+        <section class="footerone">
+            <p>Facebook</p>
+            <p>Twitter</p>
+            <p>Linkedin</p>
+        </section>
+    </footer>
+</body>
+</html>
+
